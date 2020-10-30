f99411a677509b96b3b84c824842f10d2824224f
Julee Butler <jiny2060@hotmail.com>
2020-10-30 15:46:59 -0700

    update code

--- src/components/DataCard/index.js
+++ src/components/DataCard/index.js
@@ -8,7 +8,7 @@ function DataCard(props) {
     <h5 className="card-title">{props.name.first} {props.name.last}</h5>
   </div>
   <ul className="list-group list-group-flush">
-    <li className="list-group-item">Gender {props.gender}</li>
+    <li className="list-group-item">Gender: {props.gender}</li>
     <li className="list-group-item">Email: {props.email}</li>
     <li className="list-group-item">Phone: {props.phone}</li>
     <li className="list-group-item">Location: {props.location.city}, {props.location.state}</li>
